600ad58513b510893b70f8096bae1b949e12b593
kakubotics <yashkhandelwal27@gmail.com>
2020-05-09 21:43:42 +0530

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {handleInitialData} from '../actions/shared'
+import Dashboard from './Dashboard'
+import LoadingBar from 'react-redux-loading'
+import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
+import Nav from './Nav'
+
+
+class App extends Component {
+
+  componentDidMount(){
+
+    this.props.dispatch(handleInitialData())
+
+  }
+
+  render() {
+    return (
+      <div>
+        <LoadingBar/>
+        {this.props.loading === true
+          ? null
+          : <TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}} />}
+          
+      </div>
+    )
+  }
+}
+
+function mapStateToProps ({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App) 
